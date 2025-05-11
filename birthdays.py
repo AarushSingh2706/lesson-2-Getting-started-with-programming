10829031527784f0e090f3a956109a2460211440
AarushSingh2706 <nisha2008mb0035@gmail.com>
2025-05-11 20:36:49 +0530

    birthdays

---
+++ birthdays.py
@@ -0,0 +1,13 @@
+Aarush = "27 June"
+Father = "20 December"
+Mother = "22 February"
+Nitant = "2 July"
+Advik = "5 April"
+Swaroop = "8 November"
+print("My birthday is in",Aarush)
+print("My father's birthday is in",Father)
+print("My mother's birthday is in",Mother)
+print("My friend,Nitant's birthday is in",Nitant)
+print("My friend,Advik's birthday is in",Advik)
+print("My friend,Swaroop's birthday is in" ,Swaroop, "\n")
+print("These are all the birthdays")
